12067fb5f7082ef4ffcafc8f743af8143d18175b
fmanda <fmagusta@tigaserangkai.co.id>
2023-04-14 07:29:38 +0700

    -

--- server/public/index.php
+++ server/public/index.php
@@ -4,7 +4,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Slim\Exception\HttpNotFoundException;
 
-// use Tuupola\Middleware\CorsMiddleware;
+use Tuupola\Middleware\CorsMiddleware;
 
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -17,50 +17,50 @@ $app->addRoutingMiddleware();
 
 $app->setBasePath('/public');
 
-// build https
-// $app->add(new Tuupola\Middleware\CorsMiddleware([
-//     "origin" => ["*"],
-//     "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE"],
-//     "headers.allow" => ["Authorization", "If-Match", "If-Unmodified-Since"],
-//     "headers.expose" => [],
-//     "credentials" => true,
-//     "cache" => 0,
-// ]));
+// build https 
+$app->add(new Tuupola\Middleware\CorsMiddleware([
+    "origin" => ["*"],
+    "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    "headers.allow" => ["Authorization", "If-Match", "If-Unmodified-Since"],
+    "headers.expose" => [],
+    "credentials" => true,
+    "cache" => 0,
+]));
 
 
 //
-$app->add(new Tuupola\Middleware\JwtAuthentication([
-    "regexp" => "/(.*)/", //default format Bearer <token>
-    "secret" => $config["secret"],
-    "algorithm" => ["HS256"],
-    "rules" => [
-        new Tuupola\Middleware\JwtAuthentication\RequestPathRule([
-            "ignore" => [
-              $app->getBasePath() . "/check",
-              $app->getBasePath() . "/login"
-            ]
-        ]),
-        new Tuupola\Middleware\JwtAuthentication\RequestMethodRule([
-            "ignore" => ["OPTIONS"]
-        ])
-    ]
-]));
+// $app->add(new Tuupola\Middleware\JwtAuthentication([
+//     "regexp" => "/(.*)/", //default format Bearer <token>
+//     "secret" => $config["secret"],
+//     "algorithm" => ["HS256"],
+//     "rules" => [
+//         new Tuupola\Middleware\JwtAuthentication\RequestPathRule([
+//             "ignore" => [
+//               $app->getBasePath() . "/check",
+//               $app->getBasePath() . "/login"
+//             ]
+//         ]),
+//         new Tuupola\Middleware\JwtAuthentication\RequestMethodRule([
+//             "ignore" => ["OPTIONS"]
+//         ])
+//     ]
+// ]));
 
 
 $app->options('/{routes:.+}', function ($request, $response, $args) {
     return $response;
 });
 
-// dev
-$app->add(function ($request, $handler) {
-    $response = $handler->handle($request);
-    return $response
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
-            ->withHeader("Access-Control-Allow-Credentials", "true");
-            // ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-});
+// just for dev
+// $app->add(function ($request, $handler) {
+//     $response = $handler->handle($request);
+//     return $response
+//             ->withHeader('Access-Control-Allow-Origin', '*')
+//             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+//             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
+//             ->withHeader("Access-Control-Allow-Credentials", "true");
+//             // ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+// });
 
 
 $app->get('/check', function (Request $request, Response $response, $args) {
@@ -87,3 +87,7 @@ require '../src/routes/users.php';
 // });
 
 $app->run();
+
+
+//notes upload img error : if dev cors activated and using ip 12.xx ??
+//upload img success when using corsmiddleware and ip 0.xx
